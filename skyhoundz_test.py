d471757d4d3cc7fc38894b9b0f84a5ce2f0a374f
BryanSmith <36519276+brsm11@users.noreply.github.com>
2020-10-24 22:01:54 -0400

    Lots of changes for this commit
    -Created Skyhoundz.py for all functions to scrape data from skyhoundz website
    -Merged all functions from XtremeWebScrapeFunctions.py into Skyhoundz.py
    -Created skyhoundz_test.py which utilizes Skyhoundz.py to get any kind of event data From skyhoundz website
    -Used skyhoundz_test.py to get XtremeDistance, Classic, and DiscDogathon results
    _created Offical_Data folder to store All data that is actively being used for the project, this data has not yet been cleaned and is raw from the web scraper. the next step will be to clean this data. Offical data is too large to be stored on github and will be stored at another link, which will soon be listed on the repo
    
    Co-Authored-By: Amber Simmons <70375090+drasimmons@users.noreply.github.com>

---
+++ skyhoundz_test.py
@@ -0,0 +1,29 @@
+
+"""
+Created on Sat Oct 24 18:37:10 2020
+
+@author: Bryan Smith
+"""
+
+import Skyhoundz as skypy
+import pandas as pd
+
+
+'''
+This program will get all data from the skyhoundz website 
+utilizing the Skyhoundz.py file
+'''
+outpath ='C:/Users/smith/Documents/GitHub/DIODE/Discdogathonresults.csv'
+url = "https://skyhoundz.com/previous-competition-results/"
+
+#event_wanted = 'skyhoundz-classic-results'
+#event_wanted = 'xtreme-distance-results'
+event_wanted = 'discdogathon-results'
+
+classic = skypy.Classic_WebScrape(url, event_wanted)
+classic.to_csv(outpath)
+
+
+
+
+
